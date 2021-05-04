4af564395de89d955547b96282191a613f9efe66
nonsookongwu <60108641+nonsookongwu@users.noreply.github.com>
2021-05-04 22:29:07 +0100

    second commit (Image)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -36,39 +37,42 @@ class MyPortfolioApp extends StatelessWidget {
                   ),
                 ),
               ),
+              Myimage(),
               Container(
                 height: 200,
                 width: 400,
-                alignment: Alignment.bottomCenter,
-                margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                alignment: Alignment.topCenter,
+               // margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
                 decoration: BoxDecoration(
                   color: Colors.white,
                 ),
-                child: Text(
-                  "I am Okongwu Chukwunonso, a flutter developer.",
-                  textDirection: TextDirection.ltr,
-                  style: TextStyle(
-                    color: Colors.deepPurple,
-                    fontSize: 15,
-                    fontWeight: FontWeight.normal,
+                  child: Text(
+                    "I am Okongwu Chukwunonso, a flutter developer.",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      color: Colors.deepPurple,
+                      fontSize: 15,
+                      fontWeight: FontWeight.normal,
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                height: 350,
-                width: 400,
-                alignment: Alignment.topCenter,
-                margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
-                decoration: BoxDecoration(
-                  color: Colors.deepPurple,
-                ),
-                child: Text(
-                  "Things that I am good at",
-                  textDirection: TextDirection.ltr,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 15,
-                    fontWeight: FontWeight.normal,
+              Expanded(
+                child: Container(
+                  height: 350,
+                  width: 400,
+                  alignment: Alignment.topCenter,
+                  margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                  ),
+                  child: Text(
+                    "Things that I am good at",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.normal,
+                    ),
                   ),
                 ),
               ),
@@ -79,3 +83,14 @@ class MyPortfolioApp extends StatelessWidget {
     );
   }
 }
+
+
+class Myimage extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = AssetImage("images/Untitled-1.png");
+      Image image = Image(image: assetImage, width: 250, height: 250,);
+      return Container(child: image,);
+    }
+  }
